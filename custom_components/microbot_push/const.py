9d26a5f50483d7e23dccd2d659f64c53ad8829a8
spycle <48740594+spycle@users.noreply.github.com>
2022-07-25 01:16:15 +0100

    Migrate to Bleak

--- custom_components/microbot_push/const.py
+++ custom_components/microbot_push/const.py
@@ -1,1 +1,42 @@
+"""Constants for integration_blueprint."""
+# Base component constants
+NAME = "MicroBot Push"
 DOMAIN = "microbot_push"
+DOMAIN_DATA = f"{DOMAIN}_data"
+VERSION = "2022.7.0"
+ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+ISSUE_URL = "https://github.com/spycle/microbot_push/issues"
+
+# Icons
+ICON = "mdi:toggle-switch-variant"
+
+# Device classes
+BINARY_SENSOR_DEVICE_CLASS = "connectivity"
+
+# Platforms
+BINARY_SENSOR = "binary_sensor"
+SENSOR = "sensor"
+SWITCH = "switch"
+PLATFORMS = [SWITCH]
+
+
+# Configuration and options
+CONF_ENABLED = "enabled"
+CONF_NAME = "name"
+CONF_PASSWORD = "password"
+CONF_BDADDR = "bdaddr"
+DEFAULT_TIMEOUT = 10
+
+# Defaults
+DEFAULT_NAME = "titting_about"
+
+
+STARTUP_MESSAGE = f"""
+-------------------------------------------------------------------
+{NAME}
+Version: {VERSION}
+This is a custom integration!
+If you have any issues with this you need to open an issue here:
+{ISSUE_URL}
+-------------------------------------------------------------------
+"""
