7e142823ba667cc09ccab0652fd8a5d0a6477a82
spycle <48740594+spycle@users.noreply.github.com>
2022-08-07 23:57:44 +0100

    Update to use new Bluetooth integration and adds token to Config Flow

--- custom_components/microbot_push/config_flow.py
+++ custom_components/microbot_push/config_flow.py
@@ -1,115 +1,195 @@
 """Adds config flow for MicroBot."""
-from homeassistant import config_entries
+from __future__ import annotations
+import logging
+import re
+from typing import Any
+from .api import MicroBotAdvertisement, parse_advertisement_data, MicroBotApiClient
+from homeassistant.config_entries import ConfigEntry, ConfigFlow, OptionsFlow
+from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.core import callback
-from homeassistant.helpers.aiohttp_client import async_create_clientsession
+from homeassistant.data_entry_flow import FlowResult
 import voluptuous as vol
-
-from .api import MicroBotApiClient
+from homeassistant.components import bluetooth
+from homeassistant.components.bluetooth import (
+    BluetoothServiceInfoBleak,
+    async_discovered_service_info,
+)
 from .const import (
     CONF_NAME,
     CONF_BDADDR,
     DOMAIN,
-    PLATFORMS,
+    CONF_RETRY_COUNT, 
+    DEFAULT_RETRY_COUNT, 
 )
 
-class MicroBotFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+_LOGGER: logging.Logger = logging.getLogger(__package__)
+
+def format_unique_id(address: str) -> str:
+    """Format the unique ID for a switchbot."""
+    return address.replace(":", "").lower()
+
+class MicroBotConfigFlow(ConfigFlow, domain=DOMAIN):
     """Config flow for MicroBot."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: ConfigEntry,
+    ) -> MicroBotOptionsFlowHandler:
+        """Get the options flow for this handler."""
+        return MicroBotOptionsFlowHandler(config_entry)
 
     def __init__(self):
         """Initialize."""
         self._errors = {}
+        self._discovered_adv: MicrBotAdvertisement | None = None
+        self._discovered_advs: dict[str, MicroBotAdvertisement] = {}
+        self._client: None
+        self._ble_device: None
+        self._name = None
+        self._bdaddr = None
 
-    async def async_step_user(self, user_input=None):
+    async def async_step_bluetooth(
+        self, discovery_info: BluetoothServiceInfoBleak
+    ) -> FlowResult:
+        """Handle the bluetooth discovery step."""
+        _LOGGER.debug("Discovered bluetooth device: %s", discovery_info)
+        await self.async_set_unique_id(format_unique_id(discovery_info.address))
+        self._abort_if_unique_id_configured()
+        parsed = parse_advertisement_data(
+            discovery_info.device, discovery_info.advertisement
+        )
+        self._discovered_adv = parsed
+        data = parsed.data
+        self.context["title_placeholders"] = {
+            "name": data["local_name"],
+            "address": discovery_info.address,
+        }
+        return await self.async_step_init()
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
         """Handle a flow initialized by the user."""
-        self._errors = {}
+        # This is for backwards compatibility.
+        return await self.async_step_init(user_input)
 
-        # Uncomment the next 2 lines if only a single instance of the integration is allowed:
-        # if self._async_current_entries():
-        #     return self.async_abort(reason="single_instance_allowed")
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Check if paired."""
+        errors: dict[str, str] = {}
 
-        if user_input is not None:
-#            valid = await self._test_credentials(
-#                user_input[CONF_NAME], user_input[CONF_BDADDR]
-#            )
-#            if valid:
-            return self.async_create_entry(
-                title=user_input[CONF_NAME], data=user_input
-            )
-#            else:
-#                self._errors["base"] = "auth"
-#
-#            return await self._show_config_form(user_input)
+        if discovery := self._discovered_adv:
+            self._discovered_advs[discovery.address] = discovery
+        else:
+            current_addresses = self._async_current_ids()
+            for discovery_info in async_discovered_service_info(self.hass):
+                address = discovery_info.address
+                if (
+                    format_unique_id(address) in current_addresses
+                    or address in self._discovered_advs
+                ):
+                    continue
+                parsed = parse_advertisement_data(
+                    discovery_info.device, discovery_info.advertisement
+                )
+                if parsed:
+                    self._discovered_advs[address] = parsed
 
-        user_input = {}
-        # Provide defaults for form
-        user_input[CONF_BDADDR] = ""
-        user_input[CONF_NAME] = ""
+        if not self._discovered_advs:
+            return self.async_abort(reason="no_unconfigured_devices")
 
-        return await self._show_config_form(user_input)
-
-#    @staticmethod
-#    @callback
-#    def async_get_options_flow(config_entry):
-#        return MicroBotOptionsFlowHandler(config_entry)
-
-    async def _show_config_form(self, user_input):  # pylint: disable=unused-argument
-        """Show the configuration form to edit location data."""
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_NAME, default=user_input[CONF_NAME]): str,
-                    vol.Required(CONF_BDADDR, default=user_input[CONF_BDADDR]): str,
-                }
-            ),
-            errors=self._errors,
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_BDADDR): vol.In(
+                    {
+                        address: f"{parsed.data['local_name']} ({address})"
+                        for address, parsed in self._discovered_advs.items()
+                    }
+                ),
+                vol.Required(CONF_NAME): str,
+            }
         )
 
-    async def _test_credentials(self, bdaddr):
-        """Return true if credentials is valid."""
-        try:
-#            session = async_create_clientsession(self.hass)
-            client = MicroBotApiClient(bdaddr)
-            await client.connect()
-            return True
-        except Exception:  # pylint: disable=broad-except
-            pass
-        return False
+        if user_input is not None:
+            self._name = user_input[CONF_NAME]
+            self._bdaddr = user_input[CONF_BDADDR]
+            await self.async_set_unique_id(
+                format_unique_id(self._bdaddr), raise_on_progress=False
+            )
+            self._abort_if_unique_id_configured()
+            self._ble_device = bluetooth.async_ble_device_from_address(self.hass, self._bdaddr.upper())
+            if not self._ble_device:
+                raise ConfigEntryNotReady(
+                    f"Could not find MicroBot with address {self._bdaddr}"
+            )
+            conf = self.hass.config.path()+"/.storage/microbot-"+re.sub('[^a-f0-9]', '', self._bdaddr.lower())+".conf"
+            self._client = MicroBotApiClient(
+                device=self._ble_device,
+                config=conf,
+                retry_count=DEFAULT_RETRY_COUNT,
+            )
+            token = self._client.hasToken()
+            if not token:
+                return await self.async_step_link()       
+            else:
+                return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
+        return self.async_show_form(
+            step_id="init", data_schema=data_schema, errors=errors
+        )
 
-#class MicroBotOptionsFlowHandler(config_entries.OptionsFlow):
-#    """MicroBot config flow options handler."""
+    async def async_step_link(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Given a configured host, will ask the user to press the button to pair."""
+        errors: dict[str, str] = {}
+        token = self._client.hasToken()
+        if user_input is None:
+            try:
+                await self._client.connect(init=True)
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception(
+                    "Unknown error connecting with MicroBot"
+                )
+                errors["base"] = "linking"
+            return self.async_show_form(step_id="link")
 
-#    def __init__(self, config_entry):
-#        """Initialize HACS options flow."""
-#        self.config_entry = config_entry
-#        self.options = dict(config_entry.options)
+        if not token:
+            errors:["base"] = "linking"
 
-#    async def async_step_init(self, user_input=None):  # pylint: disable=unused-argument
-#        """Manage the options."""
-#        return await self.async_step_user()
+        if errors:
+            return self.async_show_form(step_id="link", errors=errors)
 
-#    async def async_step_user(self, user_input=None):
-#        """Handle a flow initialized by the user."""
-#        if user_input is not None:
-#            self.options.update(user_input)
-#            return await self._update_options()
+        user_input[CONF_BDADDR] = self._bdaddr
 
-#        return self.async_show_form(
-#            step_id="user",
-#            data_schema=vol.Schema(
-#                {
-#                    vol.Required(x, default=self.options.get(x, True)): bool
-#                    for x in sorted(PLATFORMS)
-#                }
-#            ),
-#        )
+        return self.async_create_entry(title=self._name, data=user_input)
 
-#    async def _update_options(self):
-#        """Update config entry options."""
-#        return self.async_create_entry(
-#            title=self.config_entry.data.get(CONF_BDADDR), data=self.options
-#        )
+class MicroBotOptionsFlowHandler(OptionsFlow):
+    """Handle Microbot options."""
+
+    def __init__(self, config_entry: ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Manage Microbot options."""
+        if user_input is not None:
+            # Update common entity options for all other entities.
+            return self.async_create_entry(title="", data=user_input)
+
+        options = {
+            vol.Optional(
+                CONF_RETRY_COUNT,
+                default=self.config_entry.options.get(
+                    CONF_RETRY_COUNT, DEFAULT_RETRY_COUNT
+                ),
+            ): int
+        }
+
+        return self.async_show_form(step_id="init", data_schema=vol.Schema(options))
