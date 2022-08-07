0873fad69c5e00cf53d4fc907a4573b519f38a64
spycle <48740594+spycle@users.noreply.github.com>
2022-08-07 23:58:59 +0100

    Update to use new Bluetooth integration

--- custom_components/microbot_push/entity.py
+++ custom_components/microbot_push/entity.py
@@ -1,26 +1,48 @@
 """MicroBot class"""
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from __future__ import annotations
+from homeassistant.components.bluetooth.passive_update_coordinator import (
+    PassiveBluetoothCoordinatorEntity,
+)
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.entity import DeviceInfo
 
-from .const import DOMAIN, NAME, VERSION, ATTRIBUTION
+from .const import DOMAIN, NAME, VERSION
 
-class MicroBotEntity(CoordinatorEntity):
+class MicroBotEntity(PassiveBluetoothCoordinatorEntity):
     def __init__(self, coordinator, config_entry):
         super().__init__(coordinator)
         self.config_entry = config_entry
+        self._address = self.coordinator.ble_device.address
+        self._attr_name = "MicroBot Push",
+#        self._attr_name = self.coordinator.data["local_name"],
+        self._attr_device_info = DeviceInfo(
+            connections={(dr.CONNECTION_BLUETOOTH, self._address)},
+            manufacturer="Keymitt/Naran",
+            model="Push",
+            name="MicroBot",
+#            model=self.coordinator.data["local_name"],
+#            name=self.coordinator.data["local_name"],
+        )
         
     @property
     def unique_id(self):
         """Return a unique ID to use for this entity."""
         return self.config_entry.entry_id
 
+#    @property
+#    def device_info(self):
+#        return {
+#            "connection": {(dr.CONNECTION_BLUETOOTH, self.data["address"])},
+#            "identifiers": {(DOMAIN, self.unique_id)},
+#            "name": self.data["local_name"],
+#            "model": self.data["local_name"],
+#            "manufacturer": "Keymitt/Naran",
+#        }
+
     @property
-    def device_info(self):
-        return {
-            "identifiers": {(DOMAIN, self.unique_id)},
-            "name": NAME,
-            "model": VERSION,
-            "manufacturer": "Keymitt/Naran",
-        }
+    def data(self) -> dict[str, Any]:
+        """Return coordinator data for this entity."""
+        return self.coordinator.data
 
 #    @property
 #    def extra_state_attributes(self):
